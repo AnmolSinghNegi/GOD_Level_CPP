6507a8fcb6d9a523085daa56db937f518becb036
Anmol Singh Negi <anmolnegi242@gmail.com>
2023-11-01 02:05:59 +0530

    Directed Graph

---
+++ Graphs/Directed Graph using adjacency matrix.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m;//n=no of nodes , m=no of edges
+    int adj[n+1][n+1];
+    for(int i=0;i<=m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u][v]=1;
+    }
+    return 0;
+}
+
+//Time Complexity;-O(n), Space =O(n*n)//
